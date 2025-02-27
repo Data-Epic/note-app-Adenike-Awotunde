cda3e901f22cc2d6dab84c808383ac9e98934432
AdenikeAwotunde <138491611+AdenikeAwotunde@users.noreply.github.com>
2025-02-27 10:15:23 +0100

    modified the main loop in the note manager

--- Smart_Notes_Manager.py
+++ Smart_Notes_Manager.py
@@ -10,6 +10,8 @@ class Note:
         self.created_at = created_at
         self.content = content
         
+       
+        
     # Display Note
     def display(self):
         """ Display the content of a note """
@@ -29,11 +31,16 @@ class ReminderNote(Note):
     # A subclass for Reminder note
     def __init__(self, created_at, content, reminder_time):
         super().__init__(created_at, content)
-        self.reminder_time = reminder_time
+        
+        try:          
+            reminder_datetime = datetime.strptime(reminder_time, "%Y-%m-%d %I:%M %p")
+        except ValueError:
+            print("Invalid date/time format")
         
     def display(self):
         super().display()
         return f"ReminderNote: {self.reminder_time} | {self.created_at} ({self.content})"
+        self.reminder_time = None
     
 class NoteManager:
     # A class for note manager
@@ -62,7 +69,11 @@ class NoteManager:
     def delete_note(self, note_ID):
         # Delete a Note
         self.notes = [(id, note) for id, note in self.notes if id != note_ID]
-        print(f"Note with ID {note_ID} has been deleted")
+        if note_ID in self.notes:
+            print(f"Note with ID {note_ID} has been deleted")
+        elif note_ID not in self.notes:
+            raise ValueError("There is no note with such ID")
+        
     
     def show_note(self):
         """ Display all stored notes"""
@@ -100,7 +111,7 @@ while True:
     elif choice == "2":
         content = input("Enter reminder content: ")
         reminder_time = input("Enter reminder time (YYYY-MM-DD HH:MM AM/PM): ")
-        my_notes.add_note("Reminder", content, reminder_time=reminder_time)
+        my_notes.add_note("ReminderNote", content, reminder_time=reminder_time)
     elif choice == "3":
         my_notes.show_note()
     elif choice == "4":
--- test_Smart_Notes_Manager.py
+++ test_Smart_Notes_Manager.py
@@ -1,15 +1,61 @@
 # Import pytest and the script to test
 import pytest
-import Smart_Notes_Manager.py as Smart_Note Manager
+import datetime
+import Smart_Notes_Manager as Smart_Note
 
 class test_Note :
     def setup_method(self):
-        self.Note = Note()
+        """ set up method to initialize a class based test for the parent class"""
+        self. creatd_at = datetime(2025, 2, 27, 8, 52, 0)
+        self.content = "This is a unit testing task"
+        self.note = (self.creatd_at, self.content)
   
     def test_init(self):
-        # initialising an empty note list
+        # Testing if note list is empty at initialization
         assert len(Note) ==0
         
+    def test_display(self):
+        # Testing if the display method will return the expected formatted string
+        expected_result = f"{self.created_at} ({self.content})"
+        assert self.note.display == expected_result
+        
+    
+class test_TextNote:
+    def setup_method(self):
+        """ set up method to intiallise class based test for the subclass TextNote"""
+        self.creatd_at = datetime(2025, 2, 27, 8, 52, 0)
+        self.content = "This is a text note"
+        self.title = "Test Title"
+        self.text_note =TextNote(self.created_time, self.content, self.title)
+        
+    def test_TextNote_inherit_from_Note(self):
+        # Testing if text note is inheriting from the parent class Note
+        assert isinstance(self.text_note, Note)
+        assert isinstance(self.text_note, TextNote)
+        
+    def test_display(self):
+        # Testing if the display method will return the expected formatted string
+        expected_result = f"{self.created_at} ({self.content})"
+        assert self.note.display == expected_result
+        
+        
+class test_ReminderNote:
+    def setup_method(self):
+        """ set up method to intiallise class based test for the subclass Reminder Note"""
+        self.creatd_at = datetime(2025, 2, 27, 8, 52, 0)
+        self.content = "This is a reminder to complete your task"
+        self.reminder_time = "10am"
+        self.reminder_note = ReminderNote(self.created_time, self.content, self.reminder_time)
+        
+    def test_ReminderNote_inherit_from_Note(self):
+        # Testing if text note is inheriting from the parent class Note
+        assert isinstance(self.reminder_note_note, Note)
+        assert isinstance(self.reminder_note_note, ReminderNote)
+        
+    def test_display(self):
+        # Testing if the display method will return the expected formatted string
+        expected_result = f"ReminderNote: {self.reminder_time} | {self.created_at} ({self.content})"
+        assert self.note.display == expected_result      
                      
     
 
